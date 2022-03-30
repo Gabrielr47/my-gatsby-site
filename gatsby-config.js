112de77f167d6e87e44ca07c8a97270bf5bdc577
Gabriel Rodrigues <gabrielr47@gmail.com>
2022-03-30 15:13:31 +0200

    Finished Gatsby Tutorial Part 4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: `new`,
+    title: `My Great Gatsby Site`,
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
     {
       resolve: "gatsby-plugin-prettier-eslint",
       // this is the default configuration, override only what you need
